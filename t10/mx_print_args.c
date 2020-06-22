6af0ac631e6c6ee900179d06a037f8e8381f3418
Karyna Barinova <kbarinova@e1r1p12.unit.ua>
2020-06-22 16:14:39 +0300

    completed t10

---
+++ t10/mx_print_args.c
@@ -0,0 +1,13 @@
+int mx_strlen(const char *s);
+void mx_printchar(char c);
+void mx_printstr(const char *s);
+
+int main(int argc, char *argv[]) {
+  if (argc > 1) {
+    for (int i = 1; i < argc; i++) {
+      mx_printstr(argv[i]);
+      mx_printchar('\n');
+    }
+  }
+  return 0;
+}
