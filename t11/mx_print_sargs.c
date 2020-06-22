eb15358c492f99e5187ca02fc9b378cd0f157c80
Karyna Barinova <kbarinova@e1r1p12.unit.ua>
2020-06-22 16:44:47 +0300

    completed t11

---
+++ t11/mx_print_sargs.c
@@ -0,0 +1,25 @@
+int mx_strcmp(const char *s1, const char *s2);
+int mx_strlen(const char *s);
+void mx_printchar(char c);
+void mx_printstr(const char *s);
+
+int main(int argc, char *argv[]) {
+  char *temp;
+  if (argc > 1) {
+    for (int i = 1; i < argc; i++) {
+      for (int j = i; j < argc; j++) {
+	if (mx_strcmp(argv[i], argv[j]) > 0) {
+	  temp = argv[i];
+	  argv[i] = argv[j];
+	  argv[j] = temp;
+	}
+      }
+    }
+
+    for (int i = 1; i < argc; i++) {
+      mx_printstr(argv[i]);
+      mx_printchar('\n');
+    }
+  }
+  return 0;
+}
