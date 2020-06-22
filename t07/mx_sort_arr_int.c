3fd55d2abcf9906878f7f53559d65b2a7c3885cf
Karyna Barinova <kbarinova@e1r1p12.unit.ua>
2020-06-22 14:33:45 +0300

    completed t07

---
+++ t07/mx_sort_arr_int.c
@@ -0,0 +1,14 @@
+void mx_sort_arr_int(int *arr, int size) {
+  int temp;
+  while(size > 1) {
+    for (int i = 0; i < size; i++) {
+      for (int j = 0; j < size; j++) {
+	if (arr[i] < arr[j]) {
+	  temp = arr[i];
+	  arr[i] = arr[j];
+	  arr[j] = temp;
+	}
+      }
+    }
+  }
+}
