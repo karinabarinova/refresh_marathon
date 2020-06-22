bba4c0e383075ed2fea96c0345629b50a10fa67b
Karyna Barinova <kbarinova@e1r1p12.unit.ua>
2020-06-22 13:03:27 +0300

    completed t04

---
+++ t04/mx_printint.c
@@ -0,0 +1,12 @@
+void mx_printchar(char c);
+
+void mx_printint(int n) {
+  if (n < 0) {
+    mx_printchar('-');
+    n = -n;
+  }
+
+  if (n / 10) mx_printint(n / 10);
+
+  mx_printchar(n % 10 + '0');
+}
